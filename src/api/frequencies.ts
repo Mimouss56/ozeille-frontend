d3de8a2dc8f4ab18e50f81d78c6168668815f481
Thomas <123388670+BRC-Thomas@users.noreply.github.com>
2025-12-10 14:54:25 +0100

    api routes frequencies

---
+++ src/api/frequencies.ts
@@ -0,0 +1,50 @@
+import { axiosClient } from "../utils/axiosClient";
+
+export const getFrequencies = async (): Promise<Frequency[]> => {
+  const { data } = await axiosClient.get<Frequency[]>("/frequencies")
+  return data;
+}
+
+export const getFrequencyById = async (id: string): Promise<Frequency> => {
+  const { data } = await axiosClient.get<Frequency>(
+    `/frequencies/${id}`
+  );
+  return data;
+};
+
+export const createFrequency = async (
+  payload: CreateFrequencyDto
+): Promise<Frequency> => {
+  const { data } = await axiosClient.post<Frequency>(
+    "/frequencies",
+    payload
+  );
+  return data;
+};
+
+export const updateFrequency = async (
+  id: string,
+  payload: UpdateFrequencyDto
+): Promise<Frequency> => {
+  const { data } = await axiosClient.put<Frequency>(
+    `/frequencies/${id}`,
+    payload
+  );
+  return data;
+};
+
+export const deleteFrequency = async (id: string): Promise<void> => {
+  await axiosClient.delete(`/frequencies/${id}`);
+};
+
+export interface Frequency {
+  // TODO importer depuis shared
+}
+
+export interface CreateFrequencyDto {
+  // TODO importer depuis shared
+}
+
+export interface UpdateFrequencyDto {
+  // TODO importer depuis shared
+}
