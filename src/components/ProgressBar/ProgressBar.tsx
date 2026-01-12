5596237611ab50ec9cdfb2dbf6e1f78e962862e9
Mimouss56 <lepriol.matthieu@gmail.com>
2026-01-12 09:51:46 +0100

    lint

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,45 +1,31 @@
-import { cva, type VariantProps } from "class-variance-authority";
+import { type VariantProps, cva } from "class-variance-authority";
 
-const progressBarStyle = cva(
-    ['progress', 'w-full'],
-    {
-        variants: {
-          color: {
-            neutral: ['progress-neutral'], // Budget à 0/max
-            success: ['progress-success'], // Budget OK
-            warning: ['progress-warning'], // Budget limite)
-            error: ['progress-error'],     // Rouge Budget dépassé
-          },
-        },
-        defaultVariants: {  
-          color: 'success',
-        }
-    }
-);
+const progressBarStyle = cva(["progress", "w-full"], {
+  variants: {
+    color: {
+      neutral: ["progress-neutral"], // Budget à 0/max
+      success: ["progress-success"], // Budget OK
+      warning: ["progress-warning"], // Budget limite)
+      error: ["progress-error"], // Rouge Budget dépassé
+    },
+  },
+  defaultVariants: {
+    color: "success",
+  },
+});
 
 export type ProgressBarVariants = VariantProps<typeof progressBarStyle>;
 
-export type ProgressBarProps = React.ProgressHTMLAttributes<HTMLProgressElement> & 
+export type ProgressBarProps = React.ProgressHTMLAttributes<HTMLProgressElement> &
   ProgressBarVariants & {
     value?: number; // Valeur actuelle
     max?: number; // Valeur max (par défaut 100)
   };
 
-export const ProgressBar: React.FC<ProgressBarProps> = ({
-  className,
-  color,
-  value = 0,
-  max = 100,
-  ...props
-}) => {
+export const ProgressBar: React.FC<ProgressBarProps> = ({ className, color, value = 0, max = 100, ...props }) => {
   return (
-    <div className="w-full flex flex-col">
-      <progress
-        className={progressBarStyle({ color, className })}
-        value={value}
-        max={max}
-        {...props}
-      />
+    <div className="flex w-full flex-col">
+      <progress className={progressBarStyle({ color, className })} value={value} max={max} {...props} />
     </div>
   );
 };
