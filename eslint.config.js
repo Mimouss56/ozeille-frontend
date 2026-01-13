f7b2a8efe56be6f2700e89e7f26acc0849ae600a
Mimouss56 <lepriol.matthieu@gmail.com>
2026-01-13 15:19:18 +0100

    feat: ajouter eslint-plugin-react et mettre à jour la configuration ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -2,30 +2,35 @@ import js from "@eslint/js";
 import prettierConfig from "eslint-config-prettier";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
 import unusedImports from "eslint-plugin-unused-imports";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default defineConfig(
+export default defineConfig([
   { ignores: ["dist"] },
+
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...storybook.configs["flat/recommended"],
   jsxA11y.flatConfigs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
   prettierConfig,
   {
     files: ["**/*.{ts,tsx,js,jsx}"],
+
     plugins: {
       "unused-imports": unusedImports,
       prettier: prettier,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
+
     languageOptions: {
       ecmaVersion: "latest",
       globals: globals.browser,
@@ -36,17 +41,28 @@ export default defineConfig(
         },
       },
     },
+
+    settings: {
+      react: { version: "detect" },
+    },
+
     rules: {
       ...reactHooks.configs.recommended.rules,
+
       "prettier/prettier": "warn",
+
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "warn",
         { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
       ],
+
       "@typescript-eslint/no-unused-vars": "off",
+
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "jsx-a11y/no-autofocus": "warn",
+
+      "react/react-in-jsx-scope": "off",
+      "jsx-a11y/alt-text": "error",
     },
   },
-);
+]);
