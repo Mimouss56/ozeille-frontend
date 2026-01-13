ab1841ffdebe739e7a3d55fda15c2b889b8cf3fb
Mimouss56 <lepriol.matthieu@gmail.com>
2026-01-13 09:25:36 +0100

    feat: améliorer la gestion des erreurs dans le store utilisateur en extrayant les messages d'erreur Axios

--- src/store/user.store.ts
+++ src/store/user.store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 import type { UserStoreState } from "../@types/user.d.ts";
-import { axiosClient } from "../utils/axiosClient.ts";
+import { axiosClient, extractAxiosErrorMsg } from "../utils/axiosClient";
 
 export const useUserStore = create<UserStoreState>((set) => ({
   user: null,
@@ -13,8 +13,9 @@ export const useUserStore = create<UserStoreState>((set) => ({
       const { data } = await axiosClient.get(`/users/${id}`);
       set({ user: data, loading: false });
       return data;
-    } catch (e: any) {
-      set({ error: e?.message || "Erreur inconnue", loading: false, user: null });
+    } catch (error) {
+      const msg = extractAxiosErrorMsg(error);
+      set({ error: msg, loading: false, user: null });
       return null;
     }
   },
