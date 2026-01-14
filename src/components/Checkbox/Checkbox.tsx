d81591b23ad4af6b6cb4b930048fa5d1285d42d7
Kevin-HESSE <kev.hesse@gmail.com>
2026-01-14 13:41:55 +0100

    fix: update Checkbox component behavior.

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,12 +1,16 @@
 import { type VariantProps, cva } from "class-variance-authority";
-import React from "react";
+import React, { type ChangeEvent } from "react";
+
+import type { BaseInput } from "../InputField/type.ts";
+import { Label } from "../Label/Label.tsx";
+import { StatusMessage } from "../StatusMessage/StatusMessage.tsx";
 
 const checkboxStyle = cva(["checkbox"], {
   variants: {
     size: {
       xs: ["checkbox-xs"],
       sm: ["checkbox-sm"],
-      md: null,
+      md: ["checkbox-md"],
       lg: ["checkbox-lg"],
       xl: ["checkbox-xl"],
     },
@@ -15,11 +19,9 @@ const checkboxStyle = cva(["checkbox"], {
       secondary: ["checkbox-secondary"],
       accent: ["checkbox-accent"],
       neutral: ["checkbox-neutral"],
-    },
-    shape: {
-      square: ["rounded-none"],
-      rounded: ["rounded-md"],
-      pill: ["rounded-full"],
+      success: ["checkbox-success"],
+      info: ["checkbox-info"],
+      error: ["checkbox-error"],
     },
     disabled: {
       false: null,
@@ -28,23 +30,127 @@ const checkboxStyle = cva(["checkbox"], {
   },
   defaultVariants: {
     size: "md",
-    style: "primary",
-    shape: "rounded",
+    style: "neutral",
     disabled: false,
   },
 });
 
-export type CheckboxVariants = VariantProps<typeof checkboxStyle>;
+const toggleStyle = cva(["toggle"], {
+  variants: {
+    size: {
+      xs: ["toggle-xs"],
+      sm: ["toggle-sm"],
+      md: ["toggle-md"],
+      lg: ["toggle-lg"],
+      xl: ["toggle-xl"],
+    },
+    style: {
+      primary: ["toggle-primary"],
+      secondary: ["toggle-secondary"],
+      accent: ["toggle-accent"],
+      neutral: ["toggle-neutral"],
+      success: ["toggle-success"],
+      info: ["toggle-info"],
+      error: ["toggle-error"],
+    },
+    disabled: {
+      false: null,
+      true: null,
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    style: "neutral",
+    disabled: false,
+  },
+});
 
-export type CheckboxProps = CheckboxVariants & {
-  checked: boolean;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  name?: string;
-  id?: string;
-  label?: string;
-  "aria-label"?: string;
+type CheckboxVariants = VariantProps<typeof checkboxStyle>;
+type ToggleVariants = VariantProps<typeof toggleStyle>;
+
+type PlacedLabelProps = {
+  placement: "left" | "right";
+  rightLabel?: never;
 };
 
-export const Checkbox: React.FC<CheckboxProps> = ({ ...styleProps }) => {
-  return <input type="checkbox" className={checkboxStyle(styleProps)} disabled={styleProps.disabled || false} />;
+type PlacedSideProps = {
+  placement: "both";
+  /**
+   * Label of the left side of the toggle
+   */
+  label: string;
+  /**
+   * Label of the right side of the toggle
+   */
+  rightLabel: string;
+};
+
+type CheckboxInputProps = (CheckboxVariants | ToggleVariants) &
+  (PlacedLabelProps | PlacedSideProps) &
+  BaseInput & {
+    value: boolean;
+    /**
+     * Change the aspect of the checkbox to a toggle
+     */
+    toggleMode?: boolean;
+    /**
+     * Callback when the input value changes
+     */
+    onChange: (value: boolean) => void;
+  };
+
+export const Checkbox: React.FC<CheckboxInputProps> = ({
+  id,
+  label,
+  name,
+  value,
+  helperText,
+  readOnly,
+  onChange,
+  size,
+  style,
+  disabled,
+  placement = "right",
+  ...props
+}) => {
+  const helpTextStyle = style === "error" ? "error" : "neutral";
+
+  const componentStyle = () => {
+    return props.toggleMode ? toggleStyle({ size, style, disabled }) : checkboxStyle({ size, style, disabled });
+  };
+
+  const leftLabel = () => {
+    if (placement == "right") return null;
+    return <span>{label}</span>;
+  };
+
+  const rightLabel = () => {
+    if (placement == "left") return null;
+    if (placement == "both") return <span>{props.rightLabel}</span>;
+    return <span>{label}</span>;
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Label for="label">
+        {leftLabel()}
+        <input
+          id={id}
+          name={name}
+          type="checkbox"
+          checked={value}
+          readOnly={readOnly}
+          disabled={disabled || false}
+          className={componentStyle()}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e.target.checked)}
+        />
+        {rightLabel()}
+      </Label>
+      {helperText && (
+        <StatusMessage size="xs" style={helpTextStyle}>
+          {helperText}
+        </StatusMessage>
+      )}
+    </div>
+  );
 };
