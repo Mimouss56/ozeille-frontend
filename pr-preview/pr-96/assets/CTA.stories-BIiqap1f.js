44597b994b13abf4165c841de5f0fe0ef60b3c99
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-01-15 22:08:52 +0000

    Remove preview for PR 96 🛬

--- pr-preview/pr-96/assets/CTA.stories-BIiqap1f.js
+++
@@ -1,3 +0,0 @@
-import{C as e}from"./CTA-5H-ufXdt.js";import"./iframe-B2Nj5UWK.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DutoEiXV.js";import"./ArrowRight.esm-Bc1htG0K.js";import"./IconBase.esm-CXzRMWvT.js";const c={title:"Pages/HomePage/CTA",component:e},r={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...r.parameters?.docs?.source}}};const i=["Default"];export{r as Default,i as __namedExportsOrder,c as default};
