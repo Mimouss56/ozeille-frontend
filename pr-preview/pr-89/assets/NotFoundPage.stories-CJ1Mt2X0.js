267a1a7d37d8030c5634ce76bcc990edd49f0f38
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-01-15 09:16:40 +0000

    Remove preview for PR 89 🛬

--- pr-preview/pr-89/assets/NotFoundPage.stories-CJ1Mt2X0.js
+++
@@ -1,8 +0,0 @@
-import{j as r}from"./iframe-BHSQrC3p.js";import{B as m}from"./chunk-EPOLDU6W-DKnzOx_4.js";import{b as t}from"./Navigation-CB3NkwaZ.js";import"./preload-helper-PPVm8Dsz.js";import"./PiggyBank.es-3tO9Pdy2.js";import"./Hero-Cx5jQ7kh.js";import"./index-DutoEiXV.js";import"./StatusMessage-CvETnKzH.js";import"./Button-DQy4Di1O.js";import"./InputField-BVUNbW5Q.js";import"./Label-Bubdgq-Q.js";import"./PasswordToggle-xxBQVpzN.js";import"./IconBase.esm-DEAyzgY0.js";import"./HomePage-DMWm-nsB.js";import"./CTA-CkQFvrPc.js";import"./ArrowRight.esm-BXr0tLuC.js";import"./Features-B5Fftyj3.js";import"./Pricing-DDm2bjyM.js";import"./Security-CoFpCLAP.js";const S={title:"Pages/HomePage/Error/NotFound",component:t,tags:["autodocs"],decorators:[e=>r.jsx(m,{children:r.jsx(e,{})})]},o={render:()=>r.jsx(t,{}),parameters:{reactRouter:{routePath:"/not-found"}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <NotFoundPage />,
-  parameters: {
-    reactRouter: {
-      routePath: "/not-found"
-    }
-  }
-}`,...o.parameters?.docs?.source}}};const y=["NotFoundStory"];export{o as NotFoundStory,y as __namedExportsOrder,S as default};
--- pr-preview/pr-89/assets/formatter-QJ4M4OGQ-D_MLXCXQ.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as e}from"./blocks-C6WNYrUT.js";import{b as o}from"./iframe-BHSQrC3p.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BU8sCb2c.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
