0ea99df2dc898fba94ae60d6184ffdd14fc50403
Kevin-HESSE <Kevin-HESSE@users.noreply.github.com>
2026-01-15 13:12:30 +0000

    Deploy preview for PR 97 🛫

---
+++ pr-preview/pr-97/assets/PasswordToggle.stories-BXodTLP8.js
@@ -0,0 +1,6 @@
+import{j as o}from"./iframe-CwMXMrUU.js";import{P as s}from"./PasswordToggle-CqReHdIQ.js";import{B as a}from"./chunk-EPOLDU6W-DWpJ3nbO.js";import"./preload-helper-PPVm8Dsz.js";import"./IconBase.esm-BHyQ0JS4.js";const c={title:"UI/PasswordToggle",component:s,tags:["autodocs"],parameters:{layout:"centered"},decorators:[r=>o.jsx(a,{children:o.jsx(r,{})})]},e={args:{isVisible:!1,onToggle:()=>{}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    isVisible: false,
+    onToggle: () => {}
+  }
+}`,...e.parameters?.docs?.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,c as default};
