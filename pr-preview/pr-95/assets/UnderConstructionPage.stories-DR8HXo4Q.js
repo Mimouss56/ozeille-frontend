9105ebe567e9040b9b72d9279daa2c335b4b2295
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-01-15 11:05:14 +0000

    Remove preview for PR 95 🛬

--- pr-preview/pr-95/assets/UnderConstructionPage.stories-DR8HXo4Q.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./iframe-BXXG-kbb.js";import{M as n}from"./chunk-EPOLDU6W-BbVU8-df.js";import{U as t}from"./Navigation-BKbAxLpq.js";import"./preload-helper-PPVm8Dsz.js";import"./PiggyBank.es-RVkSKevN.js";import"./Hero-ejAfBJvD.js";import"./index-DutoEiXV.js";import"./StatusMessage-gsdkOqvg.js";import"./Button-FZonXg7I.js";import"./InputField-Dj5dUO4x.js";import"./Label-InTaDT94.js";import"./PasswordToggle-lJnJknuW.js";import"./IconBase.esm-CyXB2z-f.js";import"./HomePage-CCtO4GYA.js";import"./CTA-DBwYMmRM.js";import"./ArrowRight.esm-h0Sdj0OU.js";import"./Features-BzsKM4XQ.js";import"./Pricing-2-Cz-FHz.js";import"./Security-CdwhzF4m.js";const E={title:"Pages/HomePage/Error/UnderConstruction",component:t,tags:["autodocs"],decorators:[e=>o.jsx(n,{children:o.jsx(e,{})})]},r={render:()=>o.jsx(t,{}),parameters:{reactRouter:{routePath:"/under-construction"}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: () => <UnderConstructionPage />,
-  parameters: {
-    reactRouter: {
-      routePath: "/under-construction"
-    }
-  }
-}`,...r.parameters?.docs?.source}}};const S=["UnderConstructionStory"];export{r as UnderConstructionStory,S as __namedExportsOrder,E as default};
--- pr-preview/pr-95/assets/formatter-QJ4M4OGQ-BmQYFWtH.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as e}from"./blocks-D1PUj253.js";import{b as o}from"./iframe-BXXG-kbb.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BLNaIjIy.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
