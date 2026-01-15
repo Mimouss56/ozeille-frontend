c337dd59592aad35535fd0d93c174454b9b0899b
YouvaLo <YouvaLo@users.noreply.github.com>
2026-01-15 09:25:04 +0000

    Deploy preview for PR 93 🛫

---
+++ pr-preview/pr-93/assets/PrivateLayout.stories-CGQ5TGJh.js
@@ -0,0 +1,13 @@
+import{j as e}from"./iframe-wmKOCUGS.js";import{M as p,R as m,a as s}from"./chunk-EPOLDU6W-tDbzZP8K.js";import{H as n}from"./HomePage-D8RyajhR.js";import{P as l,a}from"./Navigation-DLPyL-Yo.js";import"./preload-helper-PPVm8Dsz.js";import"./CTA-BJPjhNdh.js";import"./index-DutoEiXV.js";import"./ArrowRight.esm-DFNwN6fW.js";import"./IconBase.esm-CTGiETPw.js";import"./Features-BerwmRP5.js";import"./Hero-BqbGrePD.js";import"./PiggyBank.es-TNisE0Iw.js";import"./Pricing-Ya7MAMd_.js";import"./Security-pGYIsp7j.js";import"./StatusMessage-xzzkp4rL.js";import"./Button-NnT647Ml.js";import"./InputField-B0FX2wXX.js";import"./Label-CUBYDiLr.js";import"./PasswordToggle-Bop2ZxB_.js";const S={title:"layouts/Private",component:l,tags:["autodocs"],globals:{viewport:{value:"desktop"}},decorators:[i=>(localStorage.setItem("token","12345"),e.jsx(p,{initialEntries:[a.PRIVATE.DASHBOARD.PATH],children:e.jsxs(m,{children:[e.jsx(s,{path:a.PUBLIC.HOME.PATH,element:e.jsx(n,{})}),e.jsx(s,{element:e.jsx(i,{}),children:Object.values(a.PRIVATE).map(t=>e.jsx(s,{path:t.PATH,element:e.jsx(t.COMPONENT,{})},t.PATH))})]})}))]},o={globals:{viewport:{value:"responsive"}}},r={globals:{viewport:{value:"iphone12"}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  globals: {
+    viewport: {
+      value: "responsive"
+    }
+  }
+}`,...o.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  globals: {
+    viewport: {
+      value: "iphone12"
+    }
+  }
+}`,...r.parameters?.docs?.source}}};const k=["Desktop","MobileView"];export{o as Desktop,r as MobileView,k as __namedExportsOrder,S as default};
