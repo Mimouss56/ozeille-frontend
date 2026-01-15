5eb761f5b079b9fbf9c2b2027eef5e10c4841ba9
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-01-15 15:55:48 +0000

    Remove preview for PR 97 🛬

--- pr-preview/pr-97/assets/PasswordToggle.stories-4HGfwdeq.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./iframe-CAmxBQr_.js";import{P as s}from"./PasswordToggle-DKeBzs4i.js";import{B as a}from"./chunk-EPOLDU6W-Dt9X0ubf.js";import"./preload-helper-PPVm8Dsz.js";import"./IconBase.esm-DHYDqEif.js";const c={title:"UI/PasswordToggle",component:s,tags:["autodocs"],parameters:{layout:"centered"},decorators:[r=>o.jsx(a,{children:o.jsx(r,{})})]},e={args:{isVisible:!1,onToggle:()=>{}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    isVisible: false,
-    onToggle: () => {}
-  }
-}`,...e.parameters?.docs?.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,c as default};
--- pr-preview/pr-97/assets/UnderConstructionPage.stories--hDhNB_P.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./iframe-CAmxBQr_.js";import{M as m}from"./chunk-EPOLDU6W-Dt9X0ubf.js";import{U as t}from"./Navigation-BGh7Pv_2.js";import"./preload-helper-PPVm8Dsz.js";import"./PiggyBank.es-DTTuBY5j.js";import"./Hero-wqfhp9_e.js";import"./index-DutoEiXV.js";import"./transactionsStore-CzL1iT2Y.js";import"./StatusMessage-RlS8qktq.js";import"./Button-DzYt55y9.js";import"./InputField-DKNYKKp-.js";import"./Label-B4cApt5f.js";import"./TransactionModal-osxij_x1.js";import"./Modal-DOdbmXTI.js";import"./Select-B1D-a03K.js";import"./IconBase.esm-DHYDqEif.js";import"./HomePage-CppWgZnP.js";import"./CTA-yYMksNJ6.js";import"./ArrowRight.esm-D3gTSxjv.js";import"./Features-ucKCnNNT.js";import"./Pricing-DSrCfh72.js";import"./Security-DVhsh0zm.js";import"./PasswordToggle-DKeBzs4i.js";import"./TransactionPage-Di7Tzl9r.js";import"./DataTable-CaPhtTTr.js";import"./ActionMenu-B1oChiz5.js";import"./TransactionDeleteModal-DdhMB-MO.js";import"./Trash.esm-Cl9h70h8.js";const v={title:"Pages/HomePage/Error/UnderConstruction",component:t,tags:["autodocs"],decorators:[e=>o.jsx(m,{children:o.jsx(e,{})})]},r={render:()=>o.jsx(t,{}),parameters:{reactRouter:{routePath:"/under-construction"}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: () => <UnderConstructionPage />,
-  parameters: {
-    reactRouter: {
-      routePath: "/under-construction"
-    }
-  }
-}`,...r.parameters?.docs?.source}}};const w=["UnderConstructionStory"];export{r as UnderConstructionStory,w as __namedExportsOrder,v as default};
