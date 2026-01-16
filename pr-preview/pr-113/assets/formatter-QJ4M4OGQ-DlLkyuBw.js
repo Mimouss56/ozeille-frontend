e20d2f36d3abb016758b92173b1f58c85d24a365
Kevin-HESSE <Kevin-HESSE@users.noreply.github.com>
2026-01-16 15:10:07 +0000

    Deploy preview for PR 113 🛫

---
+++ pr-preview/pr-113/assets/formatter-QJ4M4OGQ-DlLkyuBw.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-DS5HskNB.js";import{b as o}from"./iframe-BcF0cQhe.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CFqLgSHZ.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
