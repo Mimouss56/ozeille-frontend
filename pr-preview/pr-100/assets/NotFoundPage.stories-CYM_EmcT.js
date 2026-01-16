6880f063e01df529048e7c10b68c121bbdb0a00e
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-01-16 07:57:21 +0000

    Remove preview for PR 100 🛬

--- pr-preview/pr-100/assets/NotFoundPage.stories-CYM_EmcT.js
+++
@@ -1,8 +0,0 @@
-import{j as r}from"./iframe-B2Nj5UWK.js";import{B as m}from"./chunk-EPOLDU6W-DbDTrRq3.js";import{b as t}from"./Navigation-Dktghn8l.js";import"./preload-helper-PPVm8Dsz.js";import"./PiggyBank.es-KXzZ5fUS.js";import"./Hero-cg9d9Lgx.js";import"./index-DutoEiXV.js";import"./transactionsStore-YaxMOQus.js";import"./StatusMessage-DiH8O_0P.js";import"./Button-CHiHIdqh.js";import"./InputField-DdlmPBOs.js";import"./Label-BeY-GxIW.js";import"./TransactionModal-BPe8F9qy.js";import"./Modal-Bb1qfiAr.js";import"./Select-CX0Bdtw3.js";import"./IconBase.esm-CXzRMWvT.js";import"./HomePage-BsA_ujbP.js";import"./CTA-5H-ufXdt.js";import"./ArrowRight.esm-Bc1htG0K.js";import"./Features-Df6UOGko.js";import"./Pricing-aqRpYH_l.js";import"./Security-DWWs4td0.js";import"./PasswordToggle-BhQS3PGL.js";import"./TransactionPage-CfecCP_k.js";import"./DataTable-D2siq3oU.js";import"./ActionMenu-DecEEbQ5.js";import"./TransactionDeleteModal-DGa7Um2r.js";import"./Trash.esm-tGCi8PKs.js";const q={title:"Pages/HomePage/Error/NotFound",component:t,tags:["autodocs"],decorators:[e=>r.jsx(m,{children:r.jsx(e,{})})]},o={render:()=>r.jsx(t,{}),parameters:{reactRouter:{routePath:"/not-found"}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: () => <NotFoundPage />,
-  parameters: {
-    reactRouter: {
-      routePath: "/not-found"
-    }
-  }
-}`,...o.parameters?.docs?.source}}};const v=["NotFoundStory"];export{o as NotFoundStory,v as __namedExportsOrder,q as default};
