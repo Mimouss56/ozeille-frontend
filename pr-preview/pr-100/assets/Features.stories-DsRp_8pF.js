3e33b57a43a6b83feef30d5157d3cf8d52773d39
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-01-16 07:56:36 +0000

    Deploy preview for PR 100 🛫

---
+++ pr-preview/pr-100/assets/Features.stories-DsRp_8pF.js
@@ -0,0 +1,3 @@
+import{F as r}from"./Features-Df6UOGko.js";import"./iframe-B2Nj5UWK.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DutoEiXV.js";import"./IconBase.esm-CXzRMWvT.js";const p={title:"Pages/HomePage/Features",component:r,tags:["autodocs"]},e={args:{}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,p as default};
---
+++ pr-preview/pr-100/assets/PrivateLayout.stories-CMdPTJP9.js
@@ -0,0 +1,13 @@
+import{j as o}from"./iframe-B2Nj5UWK.js";import{M as p,R as m,a as s}from"./chunk-EPOLDU6W-DbDTrRq3.js";import{H as n}from"./HomePage-BsA_ujbP.js";import{P as l,a}from"./Navigation-Dktghn8l.js";import"./preload-helper-PPVm8Dsz.js";import"./CTA-5H-ufXdt.js";import"./index-DutoEiXV.js";import"./ArrowRight.esm-Bc1htG0K.js";import"./IconBase.esm-CXzRMWvT.js";import"./Features-Df6UOGko.js";import"./Hero-cg9d9Lgx.js";import"./PiggyBank.es-KXzZ5fUS.js";import"./Pricing-aqRpYH_l.js";import"./Security-DWWs4td0.js";import"./transactionsStore-YaxMOQus.js";import"./StatusMessage-DiH8O_0P.js";import"./Button-CHiHIdqh.js";import"./InputField-DdlmPBOs.js";import"./Label-BeY-GxIW.js";import"./TransactionModal-BPe8F9qy.js";import"./Modal-Bb1qfiAr.js";import"./Select-CX0Bdtw3.js";import"./PasswordToggle-BhQS3PGL.js";import"./TransactionPage-CfecCP_k.js";import"./DataTable-D2siq3oU.js";import"./ActionMenu-DecEEbQ5.js";import"./TransactionDeleteModal-DGa7Um2r.js";import"./Trash.esm-tGCi8PKs.js";const N={title:"layouts/Private",component:l,tags:["autodocs"],globals:{viewport:{value:"desktop"}},decorators:[i=>(localStorage.setItem("token","12345"),o.jsx(p,{initialEntries:[a.PRIVATE.DASHBOARD.PATH],children:o.jsxs(m,{children:[o.jsx(s,{path:a.PUBLIC.HOME.PATH,element:o.jsx(n,{})}),o.jsx(s,{element:o.jsx(i,{}),children:Object.values(a.PRIVATE).map(t=>o.jsx(s,{path:t.PATH,element:o.jsx(t.COMPONENT,{})},t.PATH))})]})}))]},e={globals:{viewport:{value:"responsive"}}},r={globals:{viewport:{value:"iphone12"}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  globals: {
+    viewport: {
+      value: "responsive"
+    }
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  globals: {
+    viewport: {
+      value: "iphone12"
+    }
+  }
+}`,...r.parameters?.docs?.source}}};const _=["Desktop","MobileView"];export{e as Desktop,r as MobileView,_ as __namedExportsOrder,N as default};
