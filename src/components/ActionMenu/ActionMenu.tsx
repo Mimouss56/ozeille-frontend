411d1d30af081877bda184da33fdab7580dff136
Mimouss56 <lepriol.matthieu@gmail.com>
2026-01-17 18:00:23 +0100

    fix import button

--- src/components/ActionMenu/ActionMenu.tsx
+++ src/components/ActionMenu/ActionMenu.tsx
@@ -1,11 +1,14 @@
+import type { Icon } from "@phosphor-icons/react";
 import { DotsThreeOutline } from "phosphor-react";
 import React from "react";
+
+import { Button } from "../Button/Button";
 
 export type MenuAction = {
   label: string;
   onClick: () => void;
-  icon?: React.ReactNode;
-  variant?: "default" | "danger";
+  icon?: Icon;
+  style?: "primary" | "secondary" | "outline" | "ghost" | "danger" | "dangerOutline" | "ghostOutline";
 };
 
 type ActionMenuProps = {
@@ -29,7 +32,6 @@ export const ActionMenu: React.FC<ActionMenuProps> = ({ actions }) => {
         aria-haspopup="menu"
         aria-expanded={false}
         className="hover:bg-neutral/20 focus:ring-neutral flex h-fit w-fit items-center justify-center rounded-md focus:ring-2 focus:outline-none"
-        onClick={() => {}}
         onKeyDown={(e) => {
           if (e.key === "Enter" || e.key === " ") {
             e.preventDefault();
@@ -43,16 +45,9 @@ export const ActionMenu: React.FC<ActionMenuProps> = ({ actions }) => {
         className="dropdown-content menu bg-base-100 rounded-box border-base-200 z-1 w-52 border p-2 shadow">
         {actions.map((action, index) => (
           <li key={index}>
-            <button
-              onClick={() => handleClick(action.onClick)}
-              className={
-                action.variant === "danger"
-                  ? "text-error hover:bg-error/10 focus:bg-error/10"
-                  : "focus:bg-bg-neutral/20 text-neutral"
-              }>
-              {action.icon && <span className="mr-2">{action.icon}</span>}
+            <Button size="md" onClick={() => handleClick(action.onClick)} style={action.style} icon={action.icon}>
               {action.label}
-            </button>
+            </Button>
           </li>
         ))}
       </ul>
