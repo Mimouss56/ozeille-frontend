70b713f63bafb061e644ae2116ade0c071eb41ec
BRC-Thomas <BRC-Thomas@users.noreply.github.com>
2026-01-27 18:03:52 +0000

    Remove preview for PR 120 🛬

--- pr-preview/pr-120/assets/TransactionModal.stories-CMXwVZt-.js
+++
@@ -1,9 +0,0 @@
-import{T as o}from"./TransactionModal-CpKKEyiG.js";import"./iframe-CHyB9diF.js";import"./preload-helper-PPVm8Dsz.js";import"./axiosClient-BFuGAQK-.js";import"./transactionsStore-L8WjlNl2.js";import"./InputField-JBABmD0W.js";import"./index-DutoEiXV.js";import"./Label-DlwVge5Y.js";import"./StatusMessage-Brq5kWbo.js";import"./Modal-Cue3e2ue.js";import"./Button-BmVsP7cq.js";import"./Select-CE-ZTk89.js";import"./IconBase.esm-OdkY7OTG.js";const a=[{id:"",label:"",amount:0,dueAt:new Date().toISOString(),pointedAt:null,createdAt:new Date().toISOString(),updatedAt:new Date().toISOString(),frequencyId:"",category:{id:"",label:"",budgetId:"",color:null,userId:null,limitAmount:0}},{id:"1",label:"Achat supermarché",amount:54.99,dueAt:"2026-01-10T00:00:00.000Z",pointedAt:null,createdAt:"2026-01-10T10:00:00.000Z",updatedAt:"2026-01-10T10:00:00.000Z",frequencyId:"1",category:{id:"1",label:"Alimentation",budgetId:"",color:null,userId:null,limitAmount:0}}],T={title:"UI/TransactionModal",component:o,tags:["autodocs"]},t={args:{transaction:a[0]}},r={args:{transaction:a[1]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    transaction: fakeTransaction[0]
-  }
-}`,...t.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    transaction: fakeTransaction[1]
-  }
-}`,...r.parameters?.docs?.source}}};const S=["NewForm","EditForm"];export{r as EditForm,t as NewForm,S as __namedExportsOrder,T as default};
