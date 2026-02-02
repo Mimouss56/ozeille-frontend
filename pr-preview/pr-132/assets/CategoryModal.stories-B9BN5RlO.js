37de13062edb455003f709826c956d2b67298910
BRC-Thomas <BRC-Thomas@users.noreply.github.com>
2026-02-02 12:55:53 +0000

    Remove preview for PR 132 🛬

--- pr-preview/pr-132/assets/CategoryModal.stories-B9BN5RlO.js
+++
@@ -1,9 +0,0 @@
-import{C as e}from"./CategoryModal-C9xeKNQT.js";import"./iframe-BGXKwssU.js";import"./preload-helper-PPVm8Dsz.js";import"./categoriesStore-BZdEjpPh.js";import"./IconBase.esm-B6SVvFO0.js";import"./axiosClient-Dv_Zv6cb.js";import"./InputField-DkojSiXa.js";import"./index-DutoEiXV.js";import"./Label-BdkyVd92.js";import"./StatusMessage-xT2ZOQ0I.js";import"./Modal-BO36P7Um.js";import"./index-pz7bYmGA.js";import"./index-C7LWeelI.js";import"./Button-C1SBvMSb.js";import"./Select-DeikYsKR.js";const b={title:"UI/CategoryModal",component:e,argTypes:{category:{control:"object",description:"Catégorie à éditer (undefined pour création)"}}},t={id:"cat-1",label:"Courses",budgetId:"budg-1",color:"#FF5733",limitAmount:150,userId:"user-1"},r={args:{category:void 0}},o={args:{category:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    category: undefined
-  }
-}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    category: exampleCategory
-  }
-}`,...o.parameters?.docs?.source}}};const x=["Default","EditMode"];export{r as Default,o as EditMode,x as __namedExportsOrder,b as default};
--- pr-preview/pr-132/assets/Footer.stories-bDZUvEu4.js
+++
@@ -1,3 +0,0 @@
-import{F as o}from"./Footer-BgPpIXfm.js";import"./iframe-BGXKwssU.js";import"./preload-helper-PPVm8Dsz.js";const s={title:"Pages/HomePage/Footer",component:o,tags:["autodocs"]},e={args:{}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,s as default};
