180befdff9813842103fdd1b08cc21c1a9dd8c35
YouvaLo <YouvaLo@users.noreply.github.com>
2026-02-02 14:15:17 +0000

    Remove preview for PR 130 🛬

--- pr-preview/pr-130/assets/TransactionModal.stories-CeFK3ySm.js
+++
@@ -1,9 +0,0 @@
-import{T as a}from"./TransactionModal-jSVa_GEA.js";import"./iframe-CHw8J55U.js";import"./preload-helper-PPVm8Dsz.js";import"./categoriesStore-Xzf3u5qT.js";import"./IconBase.esm-kkY_nQzP.js";import"./axiosClient-Bc8sZMcD.js";import"./frequenciesStore-BqyoDQbk.js";import"./transactionsStore-ddlRNO1M.js";import"./InputField-6oCaWKDB.js";import"./index-DutoEiXV.js";import"./Label-l3e5gGR6.js";import"./StatusMessage-BMMT4Zjg.js";import"./Modal-CIuYJ4PG.js";import"./index-BhAVPhff.js";import"./index-D8FCi3y2.js";import"./Button-DNTAG_4-.js";import"./Select-BHI-NHNr.js";const o=[{id:"",label:"",amount:0,dueAt:new Date().toISOString(),pointedAt:null,createdAt:new Date().toISOString(),updatedAt:new Date().toISOString(),frequencyId:"",category:{id:"",label:"",budgetId:"",color:null,userId:null,limitAmount:0}},{id:"1",label:"Achat supermarché",amount:54.99,dueAt:"2026-01-10T00:00:00.000Z",pointedAt:null,createdAt:"2026-01-10T10:00:00.000Z",updatedAt:"2026-01-10T10:00:00.000Z",frequencyId:"1",category:{id:"1",label:"Alimentation",budgetId:"",color:null,userId:null,limitAmount:0}}],w={title:"UI/TransactionModal",component:a,tags:["autodocs"]},t={args:{transaction:o[0]}},r={args:{transaction:o[1]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    transaction: fakeTransaction[0]
-  }
-}`,...t.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    transaction: fakeTransaction[1]
-  }
-}`,...r.parameters?.docs?.source}}};const y=["NewForm","EditForm"];export{r as EditForm,t as NewForm,y as __namedExportsOrder,w as default};
--- pr-preview/pr-130/assets/index-BhAVPhff.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-CHw8J55U.js";import{r as o}from"./index-D8FCi3y2.js";var t=o();const m=r(t);export{m as R,t as r};
