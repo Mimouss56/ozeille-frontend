82fd9284ff70a15282ec0e05b27ec93387efd050
Mimouss56 <lepriol.matthieu@gmail.com>
2026-02-02 13:16:44 +0100

    fix type transactionSChema

--- src/@types/transaction.d.ts
+++ src/@types/transaction.d.ts
@@ -2,9 +2,7 @@ import { z } from "zod";
 
 export const transactionSchema = z.object({
   label: z.string().min(1, "Le label est requis"),
-  amount: z
-    .min(1, "Le montant est requis")
-    .coerce.number({ invalid_type_error: "Le montant doit être un nombre" }),
+  amount: z.coerce.number({ invalid_type_error: "Le montant doit être un nombre" }).min(1, "Le montant est requis"),
   dueAt: z.string().min(1, "La date est requise"), // Ou .date() si vous voulez être strict sur le format
   categoryId: z.string().min(1, "Veuillez sélectionner une catégorie"),
   frequencyId: z.string().min(1, "Veuillez sélectionner une fréquence"),
