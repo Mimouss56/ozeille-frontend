2458219266b0b011e40281ad666b8b7fa973e63b
YouvaLo <YouvaLo@users.noreply.github.com>
2026-02-02 09:42:44 +0000

    Remove preview for PR 122 🛬

--- pr-preview/pr-122/assets/TransactionDeleteModal.stories-vz3d2mJO.js
+++
@@ -1,5 +0,0 @@
-import{T as a}from"./TransactionDeleteModal-CbLTVc4q.js";import"./iframe-4zMjsTVZ.js";import"./preload-helper-PPVm8Dsz.js";import"./transactionsStore-D3ax_zW8.js";import"./axiosClient-BitL05yO.js";import"./Modal-C8GXEIWm.js";import"./index-DutoEiXV.js";import"./Button-CeiL_o6U.js";import"./Trash.esm-CQdQreX9.js";import"./IconBase.esm-D4Wia5eX.js";const o={id:"1",label:"Achat supermarché",amount:54.99,dueAt:"2026-01-10T00:00:00.000Z",pointedAt:null,createdAt:"2026-01-10T10:00:00.000Z",updatedAt:"2026-01-10T10:00:00.000Z",frequencyId:"1",category:{id:"1",label:"Alimentation",budgetId:"",color:null,userId:null,limitAmount:0}},u={title:"UI/TransactionDeleteModal",component:a,tags:["autodocs"]},t={args:{transaction:o}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    transaction: fakeTransaction
-  }
-}`,...t.parameters?.docs?.source}}};const T=["Default"];export{t as Default,T as __namedExportsOrder,u as default};
