1f482912fbfc7a842f53a5257b0fed63e2499654
Youva LOUNAS <youva.lounas.97@gmail.com>
2026-02-02 10:40:39 +0100

    update zod validation for number input

--- src/@types/transaction.d.ts
+++ src/@types/transaction.d.ts
@@ -3,9 +3,8 @@ import { z } from "zod";
 export const transactionSchema = z.object({
   label: z.string().min(1, "Le label est requis"),
   amount: z
-    .string()
     .min(1, "Le montant est requis")
-    .refine((value) => !isNaN(Number(value)), "Le montant doit être un nombre"),
+    .coerce.number({ invalid_type_error: "Le montant doit être un nombre" }),
   dueAt: z.string().min(1, "La date est requise"), // Ou .date() si vous voulez être strict sur le format
   categoryId: z.string().min(1, "Veuillez sélectionner une catégorie"),
   frequencyId: z.string().min(1, "Veuillez sélectionner une fréquence"),
