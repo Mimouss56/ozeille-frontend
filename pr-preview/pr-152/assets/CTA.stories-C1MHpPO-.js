3b3277c428681d3237ee2095a43ac03e8c6280dc
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-02-03 11:09:48 +0000

    Remove preview for PR 152 🛬

--- pr-preview/pr-152/assets/CTA.stories-C1MHpPO-.js
+++
@@ -1,3 +0,0 @@
-import{C as e}from"./CTA-BIj04LJj.js";import"./iframe-DL-MgNHw.js";import"./preload-helper-PPVm8Dsz.js";import"./ArrowRight.es-DcYSw_OU.js";import"./IconBase.es-BvKbMlhj.js";import"./index-DutoEiXV.js";const c={title:"Pages/HomePage/CTA",component:e},r={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...r.parameters?.docs?.source}}};const i=["Default"];export{r as Default,i as __namedExportsOrder,c as default};
--- pr-preview/pr-152/assets/CategoryModal.stories-CnuOMP-m.js
+++
@@ -1,9 +0,0 @@
-import{C as e}from"./CategoryModal-CGZaGv4N.js";import"./iframe-DL-MgNHw.js";import"./preload-helper-PPVm8Dsz.js";import"./schemas-CpggPAYu.js";import"./axiosClient-CHWh3PjH.js";import"./coerce-Ms3eitT7.js";import"./budgetsStore-BdGhC539.js";import"./InputField-BNeBtSJ0.js";import"./index-DutoEiXV.js";import"./Label-DATkwEf7.js";import"./StatusMessage-DXFNL9x1.js";import"./Modal-urHl-m3U.js";import"./index-YUd07L6p.js";import"./index-DSiWMLvS.js";import"./Button-C3knhF_t.js";import"./Select-D3uH8uxZ.js";const x={title:"UI/CategoryModal",component:e,argTypes:{category:{control:"object",description:"Catégorie à éditer (undefined pour création)"}}},t={id:"cat-1",label:"Courses",budgetId:"budg-1",color:"#FF5733",limitAmount:150,userId:"user-1"},r={args:{category:void 0}},o={args:{category:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    category: undefined
-  }
-}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    category: exampleCategory
-  }
-}`,...o.parameters?.docs?.source}}};const E=["Default","EditMode"];export{r as Default,o as EditMode,E as __namedExportsOrder,x as default};
