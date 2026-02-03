36c470545178ba454728fec23c1a3a97a77169e8
Mimouss56 <lepriol.matthieu@gmail.com>
2026-02-03 13:39:47 +0100

    Refactor la gestion des catégories en intégrant l'état de pagination et la récupération des catégories dans le hook useCategory

--- src/pages/CategoryPage/CategoryPage.tsx
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -1,31 +1,14 @@
-import type { PaginationState } from "@tanstack/react-table";
-import { useEffect, useState } from "react";
-
 import { CategoryModal } from "../../components/CategoryModal/CategoryModal";
 import { DataTable } from "../../components/Table/DataTable";
-import { useStoreCategories } from "../../store";
 import { useCategory } from "./useCategory";
 
 export const CategoryPage = () => {
-  const { categories, fetchCategories, meta } = useStoreCategories();
-  const limit = 10;
-  // const [limit, _setLimit] = useState(10);
-  const [page, setPage] = useState<PaginationState>({
-    pageIndex: 0,
-    pageSize: limit,
-  });
-
-  const { columns } = useCategory();
-
-  useEffect(() => {
-    fetchCategories({ limit, page: page.pageIndex + 1 });
-  }, [fetchCategories, page.pageIndex]);
+  const { columns, categories, limit, page, setPage, meta } = useCategory();
 
   return (
     <div className="space-y-6">
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold text-gray-900">Gestion des catégories</h1>
-
         <CategoryModal />
       </div>
 
