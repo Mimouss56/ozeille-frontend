2461ee355b936a62c10c0c3a663de7d1253430bc
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-02-03 08:21:57 +0000

    Deploy preview for PR 143 🛫

---
+++ pr-preview/pr-143/assets/TransactionDeleteModal.stories-CBq2zw9-.js
@@ -0,0 +1,5 @@
+import{T as a}from"./TransactionDeleteModal-DxB0ZN6F.js";import"./iframe-7V8e0gjq.js";import"./preload-helper-PPVm8Dsz.js";import"./Trash.es-CWHqAn6T.js";import"./IconBase.es-Dxs4JYeP.js";import"./transactionsStore-BDGEp-4z.js";import"./axiosClient-CglRzrQP.js";import"./Modal-_2v96YSu.js";import"./index-DutoEiXV.js";import"./index-CPJAXu_-.js";import"./index-DghyEMiu.js";import"./Button-X_BnJZOn.js";const o={id:"1",label:"Achat supermarché",amount:54.99,dueAt:"2026-01-10T00:00:00.000Z",pointedAt:null,createdAt:"2026-01-10T10:00:00.000Z",updatedAt:"2026-01-10T10:00:00.000Z",frequencyId:"1",category:{id:"1",label:"Alimentation",budgetId:"",color:null,userId:null,limitAmount:0}},f={title:"UI/TransactionDeleteModal",component:a,tags:["autodocs"]},t={args:{transaction:o}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    transaction: fakeTransaction
+  }
+}`,...t.parameters?.docs?.source}}};const A=["Default"];export{t as Default,A as __namedExportsOrder,f as default};
