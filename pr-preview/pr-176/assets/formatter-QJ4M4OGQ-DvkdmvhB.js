82ba8ee4294d6d645e74597f0c0e89f8c6c1f6e1
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-02-04 15:26:17 +0000

    Deploy preview for PR 176 🛫

---
+++ pr-preview/pr-176/assets/formatter-QJ4M4OGQ-DvkdmvhB.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-Dw2vXWza.js";import{b as t}from"./iframe-BZq4Wtp5.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DpDm2x2F.js";import"./index-CtE7JSY2.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
