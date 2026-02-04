5831c848fc284895b8b4db3cd7155232bea1c6a7
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-02-04 11:37:31 +0000

    Remove preview for PR 172 🛬

--- pr-preview/pr-172/assets/PasswordToggle.stories-Cf9lNXAO.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./iframe-DqSXt728.js";import{B as s}from"./chunk-EPOLDU6W-CV6Rtrlp.js";import{P as a}from"./PasswordToggle-CgpH_Faj.js";import"./preload-helper-PPVm8Dsz.js";import"./IconBase.es-B9QtdQtA.js";const c={title:"UI/PasswordToggle",component:a,tags:["autodocs"],parameters:{layout:"centered"},decorators:[r=>o.jsx(s,{children:o.jsx(r,{})})]},e={args:{isVisible:!1,onToggle:()=>{}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    isVisible: false,
-    onToggle: () => {}
-  }
-}`,...e.parameters?.docs?.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,c as default};
--- pr-preview/pr-172/assets/index-BNmCUQGC.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-DqSXt728.js";import{r as o}from"./index-BwgytcH8.js";var t=o();const m=r(t);export{m as R,t as r};
