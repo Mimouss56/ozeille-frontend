7b0a2893728dc9d6ac9e5b0042aa7f7ce7ed9067
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-02-04 11:42:17 +0000

    Remove preview for PR 173 🛬

--- pr-preview/pr-173/assets/CTA.stories-DsY23rtP.js
+++
@@ -1,3 +0,0 @@
-import{C as e}from"./CTA-CSMyB0fN.js";import"./iframe-DqSXt728.js";import"./preload-helper-PPVm8Dsz.js";import"./ArrowRight.es-E-HjWxlt.js";import"./IconBase.es-B9QtdQtA.js";import"./index-DutoEiXV.js";const c={title:"Pages/HomePage/CTA",component:e},r={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...r.parameters?.docs?.source}}};const i=["Default"];export{r as Default,i as __namedExportsOrder,c as default};
--- pr-preview/pr-173/assets/TransactionModal.stories-CdvGZ0pv.js
+++
@@ -1,9 +0,0 @@
-import{T as a}from"./TransactionModal-Doaxwekv.js";import"./iframe-DqSXt728.js";import"./preload-helper-PPVm8Dsz.js";import"./Pencil.es-LoVeXpI_.js";import"./IconBase.es-B9QtdQtA.js";import"./InputField-DE2OsRyP.js";import"./index-DutoEiXV.js";import"./Label-Dn7WxTHt.js";import"./StatusMessage-CohKGwe6.js";import"./Modal-ACFwkXZ_.js";import"./index-BNmCUQGC.js";import"./index-BwgytcH8.js";import"./Button-DOzBj5Py.js";import"./Select-BPaTE3Ef.js";import"./schemas-nfZSsLKx.js";import"./axiosClient-DYDWA0FI.js";import"./coerce-D-j0bW5x.js";import"./frequenciesStore-_hPbFG-y.js";import"./transactionsStore-Bp_efCXk.js";const o=[{id:"",label:"",amount:0,dueAt:new Date().toISOString(),pointedAt:null,createdAt:new Date().toISOString(),updatedAt:new Date().toISOString(),frequencyId:"",category:{id:"",label:"",budgetId:"",color:null,userId:null,limitAmount:0,type:"INCOME"},categoryId:null},{id:"1",label:"Achat supermarché",amount:54.99,dueAt:"2026-01-10T00:00:00.000Z",pointedAt:null,createdAt:"2026-01-10T10:00:00.000Z",updatedAt:"2026-01-10T10:00:00.000Z",frequencyId:"1",category:{id:"1",label:"Alimentation",budgetId:"",color:null,userId:null,limitAmount:0,type:"INCOME"},categoryId:null}],w={title:"UI/TransactionModal",component:a,tags:["autodocs"]},t={args:{transaction:o[0]}},r={args:{transaction:o[1]}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    transaction: fakeTransaction[0]
-  }
-}`,...t.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    transaction: fakeTransaction[1]
-  }
-}`,...r.parameters?.docs?.source}}};const E=["NewForm","EditForm"];export{r as EditForm,t as NewForm,E as __namedExportsOrder,w as default};
