bfbb83e1d0a4407956f659497ce8d16ca1ec888a
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-02-05 09:42:30 +0000

    Remove preview for PR 191 🛬

--- pr-preview/pr-191/assets/CategoryModal.stories-Dx8widT8.js
+++
@@ -1,9 +0,0 @@
-import{C as t}from"./CategoryModal-CWpdO1Jk.js";import"./iframe-BgQ_fZKJ.js";import"./preload-helper-PPVm8Dsz.js";import"./IconBase.es-CZGrfrvE.js";import"./TrendUp.es-9MET3H27.js";import"./Button-BV9cyu97.js";import"./index-DutoEiXV.js";import"./InputField-CrXczbqr.js";import"./Label-BpmYyaN0.js";import"./StatusMessage-CJpt9Zso.js";import"./Modal-DzTZhzZr.js";import"./index-IfHfYg5i.js";import"./index-BJZ2N5o7.js";import"./Select-DCJue4gu.js";import"./schemas-D3hWGOKU.js";import"./coerce-Cd9CS-hv.js";import"./frequenciesStore-BnM9kRm6.js";import"./axiosClient-DkcN90IR.js";import"./transactionsStore-ClaYOIKs.js";const S={title:"UI/CategoryModal",component:t,argTypes:{category:{control:"object",description:"Catégorie à éditer (undefined pour création)"}}},e={id:"cat-1",label:"Courses",budgetId:"budg-1",color:"#FF5733",limitAmount:150,userId:"user-1",type:"EXPENSE"},r={args:{category:void 0}},o={args:{category:e}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    category: undefined
-  }
-}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    category: exampleCategory
-  }
-}`,...o.parameters?.docs?.source}}};const _=["Default","EditMode"];export{r as Default,o as EditMode,_ as __namedExportsOrder,S as default};
--- pr-preview/pr-191/assets/index-IfHfYg5i.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-BgQ_fZKJ.js";import{r as o}from"./index-BJZ2N5o7.js";var t=o();const m=r(t);export{m as R,t as r};
