9894ae826b6fb8d2715ea46a3fa757315a0abe41
Mimouss56 <Mimouss56@users.noreply.github.com>
2026-02-05 09:39:18 +0000

    Deploy preview for PR 191 🛫

---
+++ pr-preview/pr-191/assets/CTA.stories-DMRJ3b3q.js
@@ -0,0 +1,3 @@
+import{C as e}from"./CTA-DU8Som8F.js";import"./iframe-BgQ_fZKJ.js";import"./preload-helper-PPVm8Dsz.js";import"./ArrowRight.es-D6HqxqZp.js";import"./IconBase.es-CZGrfrvE.js";import"./index-DutoEiXV.js";const c={title:"Pages/HomePage/CTA",component:e},r={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...r.parameters?.docs?.source}}};const i=["Default"];export{r as Default,i as __namedExportsOrder,c as default};
---
+++ pr-preview/pr-191/assets/TransactionDeleteModal.stories-4pP-bHdm.js
@@ -0,0 +1,5 @@
+import{T as a}from"./TransactionDeleteModal-CzStDuPZ.js";import"./iframe-BgQ_fZKJ.js";import"./preload-helper-PPVm8Dsz.js";import"./Trash.es-BGU-xXnV.js";import"./IconBase.es-CZGrfrvE.js";import"./transactionsStore-ClaYOIKs.js";import"./axiosClient-DkcN90IR.js";import"./Modal-DzTZhzZr.js";import"./index-DutoEiXV.js";import"./index-IfHfYg5i.js";import"./index-BJZ2N5o7.js";import"./Button-BV9cyu97.js";const o={id:"1",label:"Achat supermarché",amount:54.99,dueAt:"2026-01-10T00:00:00.000Z",pointedAt:null,createdAt:"2026-01-10T10:00:00.000Z",updatedAt:"2026-01-10T10:00:00.000Z",frequencyId:"1",category:{id:"1",label:"Alimentation",budgetId:"",color:null,userId:null,limitAmount:0,type:"INCOME"},categoryId:"1"},f={title:"UI/TransactionDeleteModal",component:a,tags:["autodocs"]},t={args:{transaction:o}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    transaction: fakeTransaction
+  }
+}`,...t.parameters?.docs?.source}}};const g=["Default"];export{t as Default,g as __namedExportsOrder,f as default};
